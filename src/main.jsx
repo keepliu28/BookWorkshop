8dc7eb5874d6f817f6cc9d7fa6c1463baba107ed
keepliu28 <keepliu28@gmial.com>
2026-02-28 17:48:15 +0800

    更新主文件注释，修复注释内容以提高代码可读性

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-// src/main.jsx - 修复后
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // 确保 App.jsx 存在
+import App from './App.jsx'
 
+// 渲染进程只负责把 App 挂载到 index.html 的 root 节点上
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
